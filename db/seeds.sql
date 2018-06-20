ae96195da2fa729155994a2c2884ee1bc611f87a
Eric Baltutis <baltu008@umn.edu>
2018-06-20 17:04:10 -0500

    new files

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO burgers (name, devoured)
+VALUES ("Classic Cheeseburger", false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Texas Burger", false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Bacon and Bleu Cheese Burger", false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Mushroom and Swiss", false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Patty Melt", false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Ghost Burger", false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Umami Burger", false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ("Jucy Lucy", false);
